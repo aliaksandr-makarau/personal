3c161dc639c7818b9f44bae2052c1f8242af87a1
Aliaksandr Makarau <aliaksandr_makarau@epam.com>
2018-03-17 12:05:39 +0300

    Project for examples from the book Aditya Bhargava Grokking Algorithms was created.

---
+++ java/workspace/Bhargava_Grokking_Algorithms/src/com/amm/learning/bhargava/chapter1/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.amm.learning.bhargava.chapter1;
+
+public class ExampleTest {
+
+	public static void test() {
+		
+	}
+	
+}
---
+++ java/workspace/Bhargava_Grokking_Algorithms/src/com/amm/learning/bhargava/run/Runner.java
@@ -0,0 +1,9 @@
+package com.amm.learning.bhargava.run;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+	}
+
+}
