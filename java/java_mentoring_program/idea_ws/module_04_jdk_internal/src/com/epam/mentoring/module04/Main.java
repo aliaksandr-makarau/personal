d60742ab01f34634098181e1dea7edf78720af51
Aliaksandr Makarau <aliaksandr_makarau@epam.com>
2018-04-23 17:57:01 +0300

    Updated module 4 with Garbage collections and String deduplication.

--- java/java_mentoring_program/idea_ws/module_04_jdk_internal/src/com/epam/mentoring/module04/Main.java
+++ java/java_mentoring_program/idea_ws/module_04_jdk_internal/src/com/epam/mentoring/module04/Main.java
@@ -1,6 +1,12 @@
 package com.epam.mentoring.module04;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+
+    private static final List<String> pool = new LinkedList<>();
 
     public static void main(String[] args) {
 
@@ -53,6 +59,14 @@ public class Main {
 
         // Heap problems: fragmenting and compacting
 
+        // Garbage collection types and events:
+        // - Minor (garbage collection in young generation)
+        // - Major (garbage collection in old generation)
+        // - Full
+
+        // StopTheWorld event
+        // During minor GC all aof threads are paused.
+
         // 3. Method area
 
         // It is all about HotSpot JVM
@@ -66,7 +80,9 @@ public class Main {
         // Old objects are objects that survived after several garbage collector sessions
 
         // Heap is divided: Young Generation and Old (Tenured) Generation (YoungSize < OldSize usually)
+        // Default YoungGeneration : OldGeneration -> 1 : 2
         // Young Generation is divided: EdenSpace, Survivor1 (FromSpace) and Survivor2 (ToSpace)
+        // Default Eden : Survivor1 : Survivor2 -> 8 : 1 : 1
         // Eden for new objects, can be divided by threads but edges are smooth.
         // Survivors contain objects after few garbage collector sessions
 
@@ -86,5 +102,71 @@ public class Main {
         // - system class loader (loads classes from classpath);
         // - user-defined class loaders;
 
+        // 5. JVM Tuning
+        // - -verbose:gc Enable GC logging stdout
+        //   -XX:+PrintGCDetails
+        // - -Xms<value> Starting Heap Size: -Xms10m 10MB
+        // - -Xmx<value> Maximum Heap Size: -Xmx100m 100MB
+        // - -XX:NewRatio=<value> Ratio OldGenerationSize/YoungGenerationSize
+        //   -XX:NewSize=<value> Initial Size of Young Generation
+        //   -XX:MaxNewSize=<value> Maximum Size of Young Generation
+        //   -XX:SurvivorRation=<value> Ration Eden/Survivor
+        // - -XX:MaxGCPauseMillis=<value>
+
+        // JDK command line utilities:
+        // jstatd
+        // jps
+        // jstat
+        // jinfo
+        // jmap
+        // jstack
+
+        // JDK monitoring tools:
+        // jVisualVM ( + VisualGC plugin)
+        // JOL (Java Object Layout)
+        // HPROF (Profiling)
+
+        // GC options for JVM
+        // - -XX:+PrintGCDetails
+        // - -XX:+PrintGCTimeStamps
+        // - -Xloggc:file.log
+        // - -XX:+UseGCLogFileRotation
+        // - -XX:NumberOfGClogFiles=<N>
+
+        // GC tools to analyze:
+        // - GCViewer
+        // - GCHisto
+
+//        List<Object> objectsPool = new ArrayList<>();
+//        while (true) {
+//            objectsPool.add(new byte[10 * 1024 * 1024]);
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+        // 6. String deduplication
+        // JVM options:
+        // - -XX:+UseG1GC
+        // - -XX:+UseStringDeduplication
+        // - -XX:+PrintStringDeduplicationStatistics
+
+        int iterationCount = 0;
+        while (true) {
+            for (int i = 0; i < 100; ++i) {
+                for (int j = 0; j < 1000; ++j) {
+                    pool.add(new String("String" + j));
+                }
+            }
+
+            System.out.println("survived iteration " + iterationCount++);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
